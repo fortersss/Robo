8992ea880368969ca0cfc6c2eeabe9d144f8637b
fortersss <gutek13.13@gmail.com>
2023-03-22 21:02:57 +0100

    add active class nav, show action and routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,9 @@ Rails.application.routes.draw do
   get 'polityka-prywatnosci', to: 'static#privacy', as: :privacy
   get 'dostawa', to: 'static#shipping', as: :shipping
   get 'o-sklepie', to: 'static#about', as: :about
-  root to: "products#index"
-  resources :products, only: [:show, :index]
-  resources :categories, only: [:show]
+  root to: 'products#index'
+  resources :products, only: %i[show index]
+  resources :categories, only: %i[show]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
